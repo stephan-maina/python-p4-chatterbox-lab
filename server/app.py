3dc3be0a14b7f0726ea24d132591e0de3b88c79d
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-09-25 00:06:31 +0300

    Update app.py

--- server/app.py
+++ server/app.py
@@ -1,26 +1,40 @@
-from flask import Flask, request, make_response, jsonify
-from flask_cors import CORS
-from flask_migrate import Migrate
-
-from models import db, Message
+from flask import Flask, jsonify, request
+from models import db, TodoItem
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
-
-CORS(app)
-migrate = Migrate(app, db)
-
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
+@app.route('/api/tasks', methods=['GET'])
+def get_tasks():
+    tasks = TodoItem.query.all()
+    task_list = [task.to_dict() for task in tasks]
+    return jsonify(task_list)
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+@app.route('/api/tasks', methods=['POST'])
+def create_task():
+    data = request.json
+    new_task = TodoItem(text=data['text'], completed=False)
+    db.session.add(new_task)
+    db.session.commit()
+    return jsonify(new_task.to_dict()), 201
+
+@app.route('/api/tasks/<int:id>', methods=['PATCH', 'DELETE'])
+def manage_task(id):
+    task = TodoItem.query.get_or_404(id)
+
+    if request.method == 'PATCH':
+        data = request.json
+        task.text = data.get('text', task.text)
+        task.completed = data.get('completed', task.completed)
+        db.session.commit()
+        return jsonify(task.to_dict())
+
+    if request.method == 'DELETE':
+        db.session.delete(task)
+        db.session.commit()
+        return '', 204
 
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(debug=True)
